ca65453a63ba8732d734d927e0a5db7c95fecaef
Silas <silas.progamacao@gmail.com>
2025-11-06 07:44:57 -0300

    .

--- web/src/components/webwooks-list.tsx
+++ web/src/components/webwooks-list.tsx
@@ -2,11 +2,13 @@ import { useSuspenseInfiniteQuery } from '@tanstack/react-query'
 import { WebHooksListItem } from './webhooks-list-item'
 import { webhookListSchema } from '../http/schemas/webhooks'
 import { Loader2 } from 'lucide-react'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export function WebhooksList() {
   const loadMoreRef = useRef<HTMLDivElement>(null)
   const observerRef = useRef<IntersectionObserver>(null)
+
+  const [checkedWebhooksIds, setCheckedWebhooksIds] = useState<string[]>([])
 
   const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
     useSuspenseInfiniteQuery({
@@ -53,11 +55,28 @@ export function WebhooksList() {
     }
   }, [hasNextPage, isFetchingNextPage, fetchNextPage])
 
+  function handleWebhookChecked(webhookId: string) {
+    if (checkedWebhooksIds.includes(webhookId)) {
+      setCheckedWebhooksIds(state => {
+        return state.filter(id => id !== webhookId)
+      })
+    } else {
+      setCheckedWebhooksIds(state => [...state, webhookId])
+    }
+  }
+
+  const hasAnyWebhooks = checkedWebhooksIds.length > 0
+
   return (
-    <div className="flex-1 overflow-y-auto scrollbar-thin scrollbar-track-zinc-800 scrollbar-thumb-zinc-600 hover:scrollbar-thumb-zinc-500">
+    <div className="flex-1 overflow-y-auto scrollbar-thin scrollbar-track-zinc-800 scrollbar-thumb-zinc-600 hover:scrollbar-thumb-zinc-500 relative">
       <div className=" space-y-1 p-2">
         {webhooks.map(webhook => (
-          <WebHooksListItem key={webhook.id} webhook={webhook} />
+          <WebHooksListItem
+            key={webhook.id}
+            webhook={webhook}
+            onWebhookChecked={handleWebhookChecked}
+            isWebhookChecked={checkedWebhooksIds.includes(webhook.id)}
+          />
         ))}
       </div>
       {hasNextPage && (
@@ -69,6 +88,10 @@ export function WebhooksList() {
           )}
         </div>
       )}
+
+      <div className="fixed bottom-0 left-0 right-0 px-3">
+        <button className="w-full bg-indigo-400"> Gerar handler</button>
+      </div>
     </div>
   )
 }
